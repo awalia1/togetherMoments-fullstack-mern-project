04fe3c1c27147cef2e893d16b789bdd3b0a98aad
Amarpreet Walia <awalia@Amarpreets-MBP.fios-router.home>
2021-03-21 14:04:54 -0400

    added form and push req

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,11 +1,121 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
+import FileBase from 'react-file-base64';
+import { useDispatch } from 'react-redux';
+
 import useStyles from './styles';
+import { createPost } from '../../actions/posts';
 
 const Form = () => {
+    const [postData, setPostData] = useState({
+       creator: '',
+       title: '',
+       message: '',
+       tags: '',
+       selectedFile: '' 
+    })
+
+    const { creator, title, message, tags, selectedFile } = postData;
+
     const classes = useStyles();
+
+    const dispatch = useDispatch()
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        dispatch(createPost(postData));
+    }
+
+    const onChangeHandler = e => {
+        setPostData({...postData, [e.target.name]: e.target.value});
+    }
+
+    const onDoneHandler = base64 => {
+        setPostData({...postData, selectedFile: base64});
+    }
+
+    const clear = () => {
+        setPostData({
+            creator: '',
+            title: '',
+            message: '',
+            tags: '',
+            selectedFile: '' 
+        })
+    }
     
     return (
-        <h1>Form</h1>
+        <Paper className={classes.paper}>
+            <form 
+                autoComplete="off" 
+                noValidate 
+                className={`${classes.root} ${classes.form}`}
+                onSubmit={(e) => handleSubmit(e)}
+            >
+                <Typography variant="h6">
+                    Creating a moment together
+                </Typography>
+                <TextField 
+                    name="creator" 
+                    variant="outlined"
+                    label="Creator"
+                    fullWidth
+                    value={creator}
+                    onChange={(e) => onChangeHandler(e)}
+                />
+                <TextField 
+                    name="title" 
+                    variant="outlined"
+                    label="title"
+                    fullWidth
+                    value={title}
+                    onChange={(e) => onChangeHandler(e)}
+                />
+                <TextField 
+                    name="message" 
+                    variant="outlined"
+                    label="message"
+                    fullWidth
+                    value={message}
+                    onChange={(e) => onChangeHandler(e)}
+                />
+                <TextField 
+                    name="tags" 
+                    variant="outlined"
+                    label="tags"
+                    fullWidth
+                    value={tags}
+                    onChange={(e) => onChangeHandler(e)}
+                />
+                <div className={classes.fileInput}>
+                    <FileBase 
+                        type='file'
+                        multiple={false}
+                        onDone={(base64) => onDoneHandler(base64)}
+                    />
+                </div>
+                <Button
+                    className={classes.buttonSubmit}
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    type="submit"
+                    fullWidth
+                >
+                    Submit
+                </Button>
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    size="small"
+                    onClick={clear}
+                    fullWidth
+                >
+                    Clear
+                </Button>
+            </form>
+        </Paper>
     )
 }
 
--- server/index.js
+++ server/index.js
@@ -3,24 +3,19 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv'
+dotenv.config()
 
 import postRoutes from './routes/posts.js'
 
 const app = express();
 
-app.use(express.json({ limit: "30mmb", extended: true }));
-app.use(express.urlencoded({ limit: "30mmb", extended: true }));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-
-dotenv.config()
-
-const PORT = process.env.PORT || 5000
 
 app.use('/posts', postRoutes)
 
-app.get('/', (req, res) => {
-    res.send('homepage')
-})
+const PORT = process.env.PORT || 5000
 
 mongoose.connect(process.env.DATABASE_ACCESS , {useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
